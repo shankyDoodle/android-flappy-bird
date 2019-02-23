871beb69f384e949dc608517ecb8284f5e53dc39
skaldat1 <skaldat1@binghamton.edu>
2019-02-23 00:07:53 -0500

    initial commit

---
+++ android/src/com/shankydoodle/flappybird/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.shankydoodle.flappybird;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.shankydoodle.flappybird.StartFlappyBird;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new StartFlappyBird(), config);
+	}
+}
